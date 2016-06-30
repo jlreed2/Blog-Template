c9f693bdeac1389809e494efeee52df6e8f96d0f
Jennifer Reed <jlreed2@ufl.edu>
2016-06-29 20:52:01 -0700

    update thumbnail styling

--- index.php
+++ index.php
@@ -3,24 +3,29 @@
 <!--Begin Section Container -->
 <section class="row">
     <div class="nine columns">
+        <div class="row">
         <!--Begin Loop-->
-        <?php
-            if ( have_posts()) {
-                while ( have_posts()) {
-                    the_post(); ?>
-                    <?php 
-                        if ( has_post_thumbnail() ) {
-                            the_post_thumbnail('thumbnail');
-                        }
+        <?php if ( have_posts() ) {
+            while ( have_posts() ) {
+                the_post(); ?>
+                <?php if ( has_post_thumbnail() ) {
                     ?>
+                    <div class="three columns">
+                        <?php the_post_thumbnail('thumbnail'); ?>
+                    </div>
+                    <?php }
+                ?>
+                <div class="nine columns">
                     <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                     <?php the_excerpt(); ?>
                     <a href="<?php the_permalink(); ?>">Read More</a>
-                <?php
-                }
+                </div>
+            <?php
             }
+        }
         ?>
         <!--End Loop-->
+        </div>
     </div>
     <div class="three columns">
         <h3>Side Crap</h3>
