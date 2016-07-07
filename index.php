a7bbfb4a00e7603f2b17d4579705e6b45299b263
Jennifer Reed <jlreed2@ufl.edu>
2016-07-06 18:35:18 -0700

    Fix styling issues with secondary posts

--- index.php
+++ index.php
@@ -31,23 +31,21 @@
         <!--End Loop-->
         
         <div class="row">
-            <div class="four columns  secondary">
-                <?php if ( has_post_thumbnail() ) { ?>
-                    <?php the_post_thumbnail('thumbnail'); }?>
-                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                <h5><?php the_time('F jS, Y') ?></h5>
-            </div>
-            <div class="four columns  secondary">
-                <?php if ( has_post_thumbnail() ) { ?>
-                    <?php the_post_thumbnail('thumbnail'); }?>
-                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                <h5><?php the_time('F jS, Y') ?></h5>
-            </div>
-            <div class="four columns  secondary">
-                <?php if ( has_post_thumbnail() ) { ?>
-                    <?php the_post_thumbnail('thumbnail'); }?>
-                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                <h5><?php the_time('F jS, Y') ?></h5>
+            <div class="twelve columns">
+                <?php if ( have_posts() ) {
+                    while ( have_posts() ) {
+                    the_post(); 
+                    $loopcounter++;
+                    if ($loopcounter >= 2 ) { ?>
+                    <div class="four columns secondary">
+                        <?php if ( has_post_thumbnail() ) { ?>
+                        <?php the_post_thumbnail('thumbnail'); }?>
+                        <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+                        <h5><?php the_time('F jS, Y') ?></h5>
+                    </div>
+                <?php } 
+                    }
+                }?>
             </div>
         </div>
     </div>
